067f170866b72f51dbce319bd063445845fbe991
Zalih <veksess@gmail.com>
2026-04-01 19:34:45 +0200

    updated to 1.20.1 finished Hops Logic

--- src/main/java/net/ruusika/brewerywinery/util/BreweryWineryTags.java
+++ src/main/java/net/ruusika/brewerywinery/util/BreweryWineryTags.java
@@ -1,6 +1,7 @@
 package net.ruusika.brewerywinery.util;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
@@ -16,5 +17,17 @@ public class BreweryWineryTags {
            return TagKey.of(RegistryKeys.BLOCK, new Identifier(BreweryWinery.MOD_ID, name));
         }
 
+    }
+
+    public static final class Items {
+        public static final TagKey<Item> BEER_ADDITIVES_CROPS = createTag("beer_additives/crops");
+        public static final TagKey<Item> BEER_ADDITIVES_EDIBLE_NON_MEAT = createTag("beer_additives/edible_non_meat");
+        public static final TagKey<Item> BEER_ADDITIVES_MEAT = createTag("beer_additives/meat");
+        public static final TagKey<Item> BEER_ADDITIVES_MONSTER_FOOD = createTag("beer_additives/monster_food");
+        public static final TagKey<Item> BEER_ADDITIVES_WEIRD_MISC = createTag("beer_additives/weird_misc");
+
+        private static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, new Identifier(BreweryWinery.MOD_ID, name));
+        }
     }
 }
